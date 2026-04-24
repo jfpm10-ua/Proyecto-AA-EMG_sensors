e18b5134c525d067ff7bfda66c2c930ba1bb9e10
jfpm10-ua <perezmompean57@gmail.com>
2026-04-24 18:20:23 +0200

    Descargar dataset

--- download_dataset.py
+++ download_dataset.py
@@ -1,6 +1,16 @@
 import kagglehub
+import os
+import shutil
 
-# Download latest version
-path = kagglehub.dataset_download("kyr7plus/emg-4")
+dataset_name = "kyr7plus/emg-4"
 
-print("Path to dataset files:", path)
+download_path = kagglehub.dataset_download(dataset_name)
+
+target_path = "./data"
+
+if os.path.exists(target_path):
+    shutil.rmtree(target_path)
+
+shutil.move(download_path, target_path)
+
+print("Ruta final de los archivos del dataset:", target_path)
