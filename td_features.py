cc3b996a9befe50f77034f2b1b3f5a057209765f
Jordistt <jst33@alu.ua.es>
2026-05-15 23:49:29 +0200

    update

--- td_features.py
+++ td_features.py
@@ -4,13 +4,13 @@ from sklearn.preprocessing import StandardScaler
 
 # time-domain feature extraction (por qué elegí estas y explicarlas)
 
-def td_features_dataset(df: pd.DataFrame, scaler=StandardScaler()):
+def td_features_dataset(df: pd.DataFrame, scaler=None):
     """
-    Convierte el dataset (escalado y sin label) a métricas del dominio temporal:
+    Convierte el dataset (sin label) a métricas del dominio temporal:
     RMS, WL, MAV, SSC, ZC.
 
     Args:
-        df(pd.DataFrame): Dataframe de entrada, necesariamente escalado [-1, 1], púramente numérico.
+        df(pd.DataFrame): Dataframe de entrada, púramente numérico.
 
     Returns:
         df(pd.DataFrame): DataFrame de métricas de dominio temporal.
@@ -33,7 +33,7 @@ def td_features_dataset(df: pd.DataFrame, scaler=StandardScaler()):
     # ssc (n_f, 8, 6) la fórmula es de i=2 a N-1
     x_diff_ant = data[:,:,1:-1] - data[:,:,:-2]
     x_diff_post = data[:,:,1:-1] - data[:,:,2:]
-    ssc = np.sum((x_diff_ant*x_diff_post)>=0, axis=2)
+    ssc = np.sum((x_diff_ant*x_diff_post) < 0, axis=2)
 
     # zero-crossing
     threshold = 0.01 
@@ -42,11 +42,12 @@ def td_features_dataset(df: pd.DataFrame, scaler=StandardScaler()):
     zc_condition = (product < 0) & (diff_abs >= threshold)
     zc = np.sum(zc_condition, axis=2)
 
-    # pd.dataframe concatenado [RMS, MAV, WL, SSC, ZC]
-    df = pd.DataFrame(np.concatenate((rms, mav, wl, ssc, zc), axis=1))
-
+    # concatenado [RMS, MAV, WL, SSC, ZC]
+    result = np.concatenate((rms, mav, wl, ssc, zc), axis=1)
+    if scaler is not None:
+        result = scaler.fit_transform(result)
     # escalar por si tienen magnitudes muy diferentes
-    return scaler.fit_transform(df)
+    return pd.DataFrame(result)
 
 
 if __name__ == "__main__":
